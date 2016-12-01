33d06a727d26aa5753293584251536c58de415f7
ICA\ex02752 <arne.schoonvliet@digipolis.be>
2016-12-01 19:25:49 +0100

    update gitignore

--- webclient/app/app.module.ts
+++ webclient/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent }  from './app.component';
 
 import { AboutModule } from './about/about.module';
@@ -11,7 +11,11 @@ import { WelcomeModule } from './welcome/welcome.module';
   imports: [ 
     BrowserModule,
     WelcomeModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+            {path:'about', loadChildren: './about/about.module#AboutModule'},
+            {path: '', redirectTo: 'welcome', pathMatch: 'full' },
+            {path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+        ])
   ],
              
   declarations: [ AppComponent ],
